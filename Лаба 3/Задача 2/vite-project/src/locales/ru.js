c8fcca64bd8c9a7a6df60b0cd048c1d2bd5bedbf
akvarelka1na1ohote <166593870+akvarelka1na1ohote@users.noreply.github.com>
2025-03-27 23:31:56 +0300

    Лаба 5 (мемы)

---
+++ Лаба 3/Задача 2/vite-project/src/locales/ru.js
@@ -0,0 +1,12 @@
+
+
+export default {
+    clicks_0: 'нет кликов',
+    clicks_1: '{{count}} клик',
+    clicks_2_4: '{{count}} клика',
+    clicks_5_10: '{{count}} кликов',
+    clicks_11_20: '{{count}} кликов',
+    clicks: '{{count}} кликов',
+    reset: 'Сбросить',
+  };
+  
---
+++ Лаба 5/vite-project/src/App.tsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Homepage from "./pages/Home";
+
+
+import './App.css';
+import EditPage from "./pages/Edit";
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Meme Generator</h1>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/edit" element={<EditPage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
